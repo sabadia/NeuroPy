3abb4ad4eee2fc285e11caaf0a40f95b8f89fe3c
Mahamudul Hasan <919.hasan@gmail.com>
2019-07-27 23:08:23 +0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+ 
+setup(name = "NeuroPy",
+      version = "0.1",
+      description = "Library for interfacing with Neurosky's Mindwave EEG headset",
+      author = "Mahamudul Hasan",
+      author_email = "919.hasan@gmail.com",
+      url = "",
+      packages=['NeuroPy']
+      )
